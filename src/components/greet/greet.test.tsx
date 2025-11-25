2c14881f9e6d76e7ad9a29241f98766f48a48e4e
Atharvatathe <atharva.tathe55@gmail.com>
2025-11-25 11:45:22 +0530

    grouping test usnig describe method

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
-});
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 
-test.skip("Greet renders with name", () => {
-  render(<Greet name="John" />);
-  const textElement = screen.getByText("Hello John");
-  expect(textElement).toBeInTheDocument();
+  test("renders with name", () => {
+    render(<Greet name="John" />);
+    const textElement = screen.getByText("Hello John");
+    expect(textElement).toBeInTheDocument();
+  });
 });
