d02b49c406beb3cffb09515b16b055d6a9d19044
Atharvatathe <atharva.tathe55@gmail.com>
2025-11-25 17:40:21 +0530

    getByRole queries

---
+++ src/components/application/Apllication.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import { Application } from "./Application";
+
+describe("Application", () => {
+  test("name render correctly", () => {
+    render(<Application />);
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
+    expect(nameElement).toBeInTheDocument();
+
+    const pageHeadingElement = screen.getByRole("heading", {
+      level: 1,
+      name: "Job application form",
+    });
+    expect(pageHeadingElement).toBeInTheDocument();
+
+    const sectionHeadingElement = screen.getByRole("heading", {
+      level: 2,
+      name: "Section 1",
+    });
+    expect(sectionHeadingElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+    expect(bioElement).toBeInTheDocument();
+
+    const jobLocationElement = screen.getByRole("combobox");
+    expect(jobLocationElement).toBeInTheDocument();
+
+    const termsElement = screen.getByRole("checkbox");
+    expect(termsElement).toBeInTheDocument();
+
+    const submitButtonElement = screen.getByRole("button");
+    expect(submitButtonElement).toBeInTheDocument();
+  });
+});
