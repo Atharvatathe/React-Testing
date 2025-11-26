768816c0add7f2bfd8e4359429bc21efaa414e9f
Atharvatathe <atharva.tathe55@gmail.com>
2025-11-26 13:05:34 +0530

    queryBy method

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Application } from "./components/application/Application";
+import { Skills } from "./components/skill/Skills";
 
 function App() {
   return (
     <div className="App">
-      <Application />
+      {/* <Application /> */}
+      <Skills skills={["HTML", "CSS"]} />
     </div>
   );
 }
--- src/components/skill/Skills.test.tsx
+++ src/components/skill/Skills.test.tsx
@@ -2,17 +2,30 @@ import { render, screen } from "@testing-library/react";
 import { Skills } from "./Skills";
 
 describe("Skills", () => {
+  const skills = ["HTML", "CSS", "JavaScript"];
   test("render correctly", () => {
-    const skills = ["HTML", "CSS", "JavaScript"];
     render(<Skills skills={skills} />);
     const listItemElements = screen.getByRole("list");
     expect(listItemElements).toBeInTheDocument();
   });
 
   test("render of list items", () => {
-    const skills = ["HTML", "CSS", "JavaScript"];
     render(<Skills skills={skills} />);
     const listItemElements = screen.getAllByRole("listitem");
-    expect(listItemElements).toHaveLength(3);
+    expect(listItemElements).toHaveLength(skills.length);
+  });
+
+  test("renders login button", () => {
+    render(<Skills skills={skills} />);
+    const loginButton = screen.getByRole("button", { name: "Login" });
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  test("Start learning button is not rendered initially", () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = screen.queryByRole("button", {
+      name: "Start learning",
+    });
+    expect(startLearningButton).not.toBeInTheDocument();
   });
 });
