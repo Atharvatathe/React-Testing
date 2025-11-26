2d7dc7ca83ee438046efe8bce585e1200970337e
Atharvatathe <atharva.tathe55@gmail.com>
2025-11-26 16:52:18 +0530

    findBy query method

--- src/components/skill/Skills.test.tsx
+++ src/components/skill/Skills.test.tsx
@@ -28,4 +28,16 @@ describe("Skills", () => {
     });
     expect(startLearningButton).not.toBeInTheDocument();
   });
+
+  test("Start learning button is not rendered eventually", async () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = await screen.findByRole(
+      "button",
+      {
+        name: "Start learning",
+      },
+      { timeout: 2000 }
+    );
+    expect(startLearningButton).toBeInTheDocument();
+  });
 });
