04b493697d032dec4c456d90310b5d7244d4f164
Atharvatathe <atharva.tathe55@gmail.com>
2025-11-27 17:21:24 +0530

    custom react hook testing

---
+++ src/hooks/use-counter/useCounter.test.tsx
@@ -0,0 +1,16 @@
+import { renderHook } from "@testing-library/react";
+import { useCounter } from "./useCounter";
+
+describe("useCounter", () => {
+  test("should render the initial count", () => {
+    const { result } = renderHook(useCounter);
+    expect(result.current.count).toBe(0);
+  });
+
+  test("should render and accept same initial count", () => {
+    const { result } = renderHook(useCounter, {
+      initialProps: { initialCount: 10 },
+    });
+    expect(result.current.count).toBe(10);
+  });
+});
