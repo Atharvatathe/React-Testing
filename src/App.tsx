fadaeed0278592f926d88f1c2d31c5cc25a0817c
Atharvatathe <atharva.tathe55@gmail.com>
2025-11-27 13:57:48 +0530

    Testing providers

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import "./App.css";
 import { Application } from "./components/application/Application";
 import { Skills } from "./components/skill/Skills";
 import { Counter } from "./components/counter/Counter";
+import { MuiMode } from "./components/mui/MuiMode";
+import { AppProviders } from "./providers/AppProviders";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Application /> */}
-      {/* <Skills skills={["HTML", "CSS"]} /> */}
-      <Counter />
-    </div>
+    <AppProviders>
+      <div className="App">
+        {/* <Application /> */}
+        {/* <Skills skills={["HTML", "CSS"]} /> */}
+        {/* <Counter /> */}
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
