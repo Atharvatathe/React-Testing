1b9782576805ccfcf91ef33341fe628546f02c86
Atharvatathe <atharva.tathe55@gmail.com>
2025-12-03 18:32:20 +0530

    MSW testing - error handling

--- src/components/users/Users.test.tsx
+++ src/components/users/Users.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { Users } from "./Users";
-// import { rest } from "msw";
+import { http } from "msw";
 import { server } from "../../mocks/server";
 
 describe("Users Component", () => {
@@ -14,5 +14,16 @@ describe("Users Component", () => {
     render(<Users />);
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(3);
+  });
+
+  test("render error", async () => {
+    server.use(
+      http.get("https://jsonplaceholder.typicode.com/users", () => {
+        return new Response("Internal Server Error", { status: 500 });
+      })
+    );
+    render(<Users />);
+    const errorMessage = await screen.findByText("Error fetching users");
+    expect(errorMessage).toBeInTheDocument();
   });
 });
